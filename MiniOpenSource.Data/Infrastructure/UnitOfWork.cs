e95f57c7358690e40610971f19434c453f144dcf
DESKTOP-LTUG14J\user <nguyenhuukhanhphat@gmail.com>
2020-11-15 00:12:13 +0700

    commit trien khai repository va dbcontext

---
+++ MiniOpenSource.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOpenSource.Data.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+        private readonly IDBFactory dbFactory;
+        private MiniOSDbContext dbContext;
+
+        public UnitOfWork(IDBFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public MiniOSDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
