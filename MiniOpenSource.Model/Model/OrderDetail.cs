8a62619b2642df17993db7a60e26fc9104db7537
DESKTOP-LTUG14J\user <nguyenhuukhanhphat@gmail.com>
2020-11-14 22:26:10 +0700

    upload tang model

---
+++ MiniOpenSource.Model/Model/OrderDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOpenSource.Model.Model
+{
+	[Table("OrderDetails")]
+	public class OrderDetail
+	{
+		[Key]
+		[Column(Order = 1)]
+		public int OrderID { get; set; }
+
+		[Key]
+		[Column(Order = 2)]
+		public int ProductID { get; set; }
+
+		public int Quantity { get; set; }
+
+		public decimal Price { set; get; }
+
+		[ForeignKey("OrderID")]
+		public virtual Order Order { set; get; }
+
+		[ForeignKey("ProductID")]
+		public virtual Product Product { set; get; }
+	}
+}
---
+++ MiniOpenSource.Model/Model/VisitorStatic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOpenSource.Model.Model
+{
+	[Table("VisitorStatics")]
+	public class VisitorStatic
+	{
+		[Key]
+		public Guid ID { set; get; }
+
+		[Required]
+		public DateTime VisitedDate { set; get; }
+
+		[MaxLength(50)]
+		public string IPAddress { set; get; }
+	}
+}
