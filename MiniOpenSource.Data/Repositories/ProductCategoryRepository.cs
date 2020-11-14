bd054c643347c1626cf679395a2035966f6a24af
DESKTOP-LTUG14J\user <nguyenhuukhanhphat@gmail.com>
2020-11-15 00:35:06 +0700

    trien khai repository

---
+++ MiniOpenSource.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using MiniOpenSource.Data.Infrastructure;
+using MiniOpenSource.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOpenSource.Data.Repositories
+{
+	//de dinh nghia cac phuong thuc khi them
+	public interface IProductCategoryRepository : IRepository<ProductCategory>
+	{
+		IEnumerable<ProductCategory> GetByAlias(string alias);
+	}
+	public class ProductCategoryRepository : RepositoryBase<ProductCategory>
+	{
+		public ProductCategoryRepository(IDBFactory dbFactory)
+			: base(dbFactory)
+		{
+		}
+		public IEnumerable<ProductCategory> GetByAlias(string alias)
+		{
+			return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+		}
+	}
+}
